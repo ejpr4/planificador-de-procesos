2087e1480109b4ea598649db222c563cbdedf774
Elvis Páez <elvispaez18@gmail.com>
2021-07-27 22:36:58 -0400

    avance

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,70 @@
 <template>
-  <hello-world />
+  <v-container class="pt-12">
+    <v-row class="text-center">
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Bienvenidos
+        </h1>
+
+        <p class="subheading font-weight-regular mt-8">
+          Este es un simulador de planificador de procesos <br>
+          a corto plazo
+        </p> 
+        
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-3">
+          Selecciona un algoritmo de planificación
+        </h2>
+
+        <v-row justify="center" class="mt-6">
+          <v-card
+            v-for="(option, i) in options"
+            :key="i"
+            :to="option.href"
+            class="subheading mx-3 py-2 my-4"
+            link
+            :color="option.background"
+          >
+          <v-card-content class="white--text text-subtitle-1 font-weight-medium pa-4">
+            
+            {{ option.text }}
+            </v-card-content>
+          </v-card>
+        </v-row>
+      </v-col>
+
+    </v-row>
+    <p class="caption text-center">Desarrollado por: Elvis J. Páez R.<br> c.i: 24.853.972</p>
+  </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
   export default {
-    name: 'Home',
+    name: 'HelloWorld',
 
-    components: {
-      HelloWorld,
-    },
+    data: () => ({
+      options: [
+       {
+        text:'Fifo',
+        background: 'success',
+        href: '/fifo'
+      },
+      {
+        text:'Segundo Elemento',
+        background: 'warning',
+        href: '#'
+      },
+      {
+        text:'Tercer Elemento',
+        background: 'indigo',
+        href: '#'
+      }
+      ],
+    }),
   }
 </script>
---
+++ src/views/fifo.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col>
+      <h1>
+        Algoritmo Fifo (First-in-first-out)
+      </h1>
+      </v-col>
+      <v-col cols="12">
+
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      process_ready:[
+        {
+          id:1,
+          t_exe: 4,
+          t_arr: 0,
+          block: ''
+        },
+        {
+          id:2,
+          t_exe: 3,
+          t_arr: 2,
+          block: ''
+        },
+        {
+          id:3,
+          t_exe: 5,
+          t_arr: 3,
+          block: ''
+        },{
+          id:4,
+          t_exe: 6,
+          t_arr: 5,
+          block: ''
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
